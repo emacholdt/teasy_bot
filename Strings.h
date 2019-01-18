6cedbb1bd76c777de3a7ef9a756bb92fe117fbcc
emacholdt <rico2na@gmail.com>
2019-01-18 18:56:33 +0100

    outsoruced String variables for LCD

---
+++ Strings.h
@@ -0,0 +1,43 @@
+/*
+collection of String arrays for ENG and GER
+
+*/
+
+
+String welcomeString[] =   {"welcome", "Willkommen"};
+String greenTeaString[] =   {"Green tea", "Gr\365ntee"};
+String blackTeaString[] =   {"Black tea", "Schwarztee"};
+String herbalTeaString[] =   {"Herbal tea", "Kr\341utertee"};
+String fruitTeaString[] =   {"Fruit tea", "Fr\365chtetee"};
+String whiteTeaString[] =   {"White tea", "Wei\342er Tee"};
+String batteryStatusString[] =   {"battery status:", "Akkustand:"};
+String batteryEmptyString[] =   {"empty", "leer"};
+String batteryMediumString[] =   {"medium", "mittel"};
+String batteryFullString[] =   {"full", "voll"};
+String chooseTeaString[] =   {"choose tea", "Teeauswahl"};
+String chooseTimeString[] =   {"choose time", "Zeitauswahl"};
+String minutesString[] =   {"minutes", "Minuten"};
+String infuseTimeString[] =   {"infuse time", "Ziehzeit"};
+String numberOfDipsString[] =   {"number of dips", "Tauchenvorg\341nge"};
+String noneString[] =   {"none", "keine"};
+String startingString[] =   {"Starting", "Starten"};
+String timeRemainingString[] =   {"time remaining", "Restzeit"};
+String succesMessage1String[] =   {"the tea is ready", "Tee ist fertig!"};
+String succesMessage2String[] =   {"     enjoy!", "Guten Appetit"};
+
+
+/* 
+String String[] =   {"", ""};
+String String[] =   {"", ""};
+String String[] =   {"", ""};
+String String[] =   {"", ""};
+String String[] =   {"", ""};
+
+Umlaute:
+
+ \341  ä
+ \342  ß
+ \357  ö
+ \365  ü
+
+*/
