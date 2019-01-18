7ff18a4a610082d0143cf938c57ba6f5d10d2b4a
emacholdt <rico2na@gmail.com>
2019-01-18 18:56:51 +0100

    outsoruced tea dictionary

---
+++ teas.h
@@ -0,0 +1,21 @@
+/* 
+create tea dictionary 		Teelexikon erstellen
+
+Umlaute:
+
+ \341  ä
+ \342  ß
+ \357  ö
+ \365  ü
+
+*/
+
+String teas[][3] =   
+{
+  {"Green tea", "Gr\365ntee", "2"},
+  {"Black tea", "Schwarztee", "4"},
+  {"Herbal tea", "Kr\341utertee", "6"},
+  {"Fruit tea", "Fr\365chtetee", "8"},
+  {"White tea", "Wei\342er Tee", "2"}
+};
+
